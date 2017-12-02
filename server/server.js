85fb4026ca1ba0f55be462076278a294d63bb225
Arvind Kasale <arvind.kasale@appdirect.com>
2017-12-02 19:02:49 +0530

    Patch support

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -63,6 +65,33 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400);
         res.send({errorMessage: 'Invalid request. No ID sent'});
     }
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if(!ObjectID.isValid(id)) {
+        res.status(400).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    }).then((todo) => {
+        if(!todo) {
+            res.status(404).send();
+        } else {
+            res.send({todo});
+        }
+    }, (e) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(port, (err) => {
