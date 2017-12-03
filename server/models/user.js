fc927a00560b4ded5615b835d140ef77c6083d21
Arvind Kasale <arvind.kasale@appdirect.com>
2017-12-03 11:32:22 +0530

    Authentication post login token

--- server/models/user.js
+++ server/models/user.js
@@ -61,6 +61,24 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject('No user found');
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                } else {
+                    reject('Invalid email and password');
+                }
+            })
+        })
+    });
+};
+
 UserSchema.methods.toJSON = function() {
     var user = this;
     var userObject = user.toObject();
